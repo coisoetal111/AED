2f30c02110ec2c5510414503ee4529ebe77ca368
coisoetal111 <coisoetal111@gmail.com>
2026-09-16 22:15:04 +0100

    Update healkristin.c

--- projeto/healkristin.c
+++ projeto/healkristin.c
@@ -51,34 +51,34 @@ int cities = 0;
     }
     
  }
- if(map == NULL || position == NULL || quests == NULL) exit(EXIT_FAILURE); 
+ if(map == NULL || position == NULL || quests == NULL || results == NULL) exit(EXIT_FAILURE); 
  
  //contagem do número de cidades
- int ch;
- while((ch = fgetc(position)) != EOF){
-    if(ch == '\n') cities ++;
- }
- rewind(position);
 
- //agrupamento em clusters (Task1)
+ int operations;
+ fscanf(map, "%d %d", &cities, &operations);
+
+ //agrupamento em clusters
  int clusters[cities];
- for(int i = 0; i < cities; i++) clusters[i] = i;
- int i = 0;
+ for(int i = 0; i < cities; i++) clusters[i] = i + 1;
+ 
  int p,q,r;
- while(fscanf(map, "%d %d", &p, &q) == 2){
-    if(clusters[p] == clusters[q]) continue; 
-    r = clusters[q];
-    for(int i = 0; i < cities; i++) if(clusters[i] == r) clusters[i] = clusters[p];
+    
+ for(int i = 0; i < operations; i++){
+    if(fscanf(map, "%d %d", &p, &q) != 2) exit(EXIT_FAILURE);
+    if(clusters[p - 1] == clusters[q - 1]) continue; 
+    r = clusters[q - 1];
+    for(int i = 0; i < cities; i++) if(clusters[i] == r) clusters[i] = clusters[p - 1];
  }
  rewind(map);
   
- //cluster counter
+ //cluster counter (Task1)
  int temp[cities];
  for (int i = 0; i < cities; i++) temp[i] = 0;
  int temp_ = 0;
 
  int cluster_couter = 1;
- for(int i = 2; i < cities; i++) if(clusters[1] != clusters[i]){ 
+ for(int i = 1; i < cities; i++) if(clusters[0] != clusters[i]){ 
     
 
     if(temp_ == 0){
@@ -89,17 +89,19 @@ int cities = 0;
         for(int j = 0; j < temp_; j++){
             if(temp[j] == clusters[i]){
                 continue;
+            }else if(j == temp_ -1){
+                temp[temp_] = clusters[i];
+                temp_ ++;
+                cluster_couter++;
             }
-            temp[temp_] = clusters[i];
-            temp_ ++;
-            cluster_couter++;
         }
+        
     }
     
 
 }
 
-fprintf(results, "Task1 %d", cluster_couter);
+fprintf(results, "\nTask1 %d", cluster_couter);
  
  
 
